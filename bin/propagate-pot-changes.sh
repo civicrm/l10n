b7c221aa03a92aa8f833ad30b2dda784f50774d7
Piotr Szotkowski <chastell@chastell.net>
2011-03-28 14:57:30 +0200

    be a bit more quieter, my friend

--- bin/propagate-pot-changes.sh
+++ bin/propagate-pot-changes.sh
@@ -4,8 +4,8 @@ langs=`ls -1d po/?? po/??_?? | cut -d/ -f2`
 pots=`for pot in po/pot/*.pot; do basename $pot .pot; done`
 
 for lang in $langs; do
-  echo; echo $lang
+  echo $lang
   for pot in $pots; do
-    msgmerge --unique po/$lang/$pot.po po/pot/$pot.pot
+    msgmerge --no-fuzzy-matching --quiet --update po/$lang/$pot.po po/pot/$pot.pot
   done
 done
