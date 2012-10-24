4915603f48adc15d5732f7d981af8015bfa46f33
Mathieu Lutfy <mathieu@bidon.ca>
2012-10-24 18:47:57 -0400

    po: update bin/extractor.php (pulled from Transifex.net by CiviCRM l10n maintainer)

--- bin/extractor.php
+++ bin/extractor.php
@@ -67,6 +67,7 @@ $smartyExtractor = dirname(__FILE__) . '/smarty-extractor.php';
 $dir = $argv[2];
 
 $command = "find $dir/CRM $dir/packages/HTML/QuickForm $phpModifier -not -wholename '*/CRM/Core/I18n.php' -not -wholename '*/CRM/Core/Smarty/plugins/block.ts.php' | grep -v '/\.svn/' | sort | xargs $phpExtractor $dir";
+fwrite(STDERR, "Running: $command\n");
 $phpPot = `$command`;
 
 $command = "find $dir/templates $dir/xml $smartyModifier | grep -v '/\.svn/' | sort | xargs $smartyExtractor $dir";
