e2639c120e334b5583d727b433e0a971fe818fea
Mathieu Lutfy <mathieu@bidon.ca>
2013-05-30 15:02:23 -0400

    Added an option to generate only a .mo for a single language, + added help/usage.

--- bin/compile-mo-files.sh
+++ bin/compile-mo-files.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
-langs=`ls -1d po/?? po/??_?? | cut -d/ -f2`
+function usage() {
+  cat <<EOT
+compile-mo-files.sh - compiles the .po files to binary .mo format
+At runtime, gettext uses the compiled .mo format to translate strings (faster).
 
-for lang in $langs; do
-  echo "compiling $lang"
+Usage:
+
+  ./bin/compile-mo-files.sh [locale]
+
+Example:
+
+  ./bin/compile-mo-files.sh
+  ./bin/compile-mo-files.sh fr
+
+EOT
+
+  exit 1;
+}
+
+function compile_po () {
+  lang=$1
+
+  echo -n "compiling $lang ... "
   msgcat --use-first po/$lang/*.po | msgfmt -o po/$lang/civicrm.mo -
-done
+  echo "done"
+}
+
+if [ "$1" = "--help" -o "$1" = "-h" ]; then
+  usage
+fi
+
+if [ -n "$1" ]; then
+  if [ -d "po/$1" ]; then
+    compile_po $1
+  else
+    echo "Error: $1: invalid locale (not found in the ./po/ directory)."
+    exit 1
+  fi
+else
+  langs=`ls -1d po/?? po/??_?? | cut -d/ -f2`
+  
+  for lang in $langs; do
+    compile_po $lang
+  done
+fi
+
