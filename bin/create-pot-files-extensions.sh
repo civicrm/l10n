d6a5db767e78d6921608d73b427184893ae9efe8
Mathieu Lutfy <mathieu@bidon.ca>
2013-10-12 05:28:42 -0400

    Cleanup code and make it easier to run from the extension's working directory without having to specify any arguments.

--- bin/create-pot-files-extensions.sh
+++ bin/create-pot-files-extensions.sh
@@ -8,7 +8,16 @@ Usage:
   ./bin/create-pot-files-extensions.sh [short name] [src dir] [dest dir]
 
 Example:
+  ./bin/create-pot-files-extensions.sh
   ./bin/create-pot-files-extensions.sh i18example ~/repository/ca.bidon.i18nexample/  ~/repository/ca.bidon.i18nexample/po/pots/
+
+In the recommended use-case, you should run this command when your working
+directory is the base directory of your extension, e.g. where the info.xml
+is located.
+
+The .pot file will be placed in l10n/pot/{extname}.pot.
+Translations should be located in, for example, l10n/fr_FR/LC_MESSAGES/{extname}.po
+and be compiled as l10n/fr_FR/LC_MESSAGES/{extname}.mo
 
 NB: this program uses the "sponge" command, which you can get by installing the
 "moreutils" package under Debian/Ubuntu.
@@ -21,57 +30,72 @@ EOT
 [ "$1" == "--help" ] && usage
 [ "$1" == "-h" ] && usage
 
-[ "$1" == "" ] && echo 'extension name missing' && usage
-
-[ "$2" == "" ] && echo 'source dir missing'     && usage
-test ! -e "$2" && echo 'source does not exist'  && usage
-test ! -d "$2" && echo 'source not a directory' && usage
-
-[ "$3" == "" ] && echo 'target dir missing'     && usage
-test ! -e "$3" && echo 'target does not exist (please create it if necessary)'  && usage
-test ! -d "$3" && echo 'target not a directory' && usage
-
-# Test whether the sponge command is installed
-which sponge > /dev/null
-if [ $? -ne 0 ]; then
-  echo "Could not find the sponge command."
-  usage
-fi
-
 extname="$1"
 root="$2"
 potdir="$3"
 
+# In the typical use-case, we are invoked from the working directory of the extension
+# and we can use the info.xml to get the extension name
+if [ "$1" == "" ]; then
+  test ! -f "info.xml" && echo "Error: Could not find info.xml in the current directory `pwd`" && usage
+
+  # Extract the tag "<file>i18nexample</file>" from the info.xml
+  # We first extract the line with the "file" tag, then extract the 'file' value.
+  # the regexp is a bit more generous than necessary, in case everything is on one line.
+  extname=`grep '<file>' info.xml | sed 's|\s*<file>\(.*\)</file>\s*|\1|'`
+
+  test "$extname" == "" && echo 'Error: could not find the file tag from info.xml' && usage
+
+  root="."
+  potdir="l10n/pot/"
+
+  # At this point we can assume that we are in the right place, and can create
+  # the pot target directory if necessary.
+  if [ ! -d "$potdir" ]; then
+    echo " * Creating target pot directory: $potdir"
+    mkdir -p "$potdir"
+  fi
+fi
+
+# Ensure that the source and target directory exist
+test ! -e "$root" && echo "Error: $root source does not exist"  && usage
+test ! -d "$root" && echo "Error: $root source not a directory" && usage
+
+test ! -e "$potdir" && echo "Error: $potdir target does not exist (please create it if necessary)" && usage
+test ! -d "$potdir" && echo "Error: $portidr target not a directory" && usage
+
+# Test whether the sponge command is installed
+which sponge > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Could not find the sponge command. sudo apt-get install moreutils ?"
+  usage
+fi
+
 header=`tempfile`
 now=`date +'%F %R%z'`
-sed "s/NOW/$now/" `dirname $0`/header > $header
 
-# $componets are the dirs with strings generating per-component $component.po files
-# components=`ls -1 $root/CRM $root/templates/CRM | grep -v :$ | grep -v ^$ | grep -viFf bin/basedirs | sort -u | xargs | tr [A-Z] [a-z]`
-
-# Helper function for extracting strings from xml/templates
-# which are mostly component-specific. We also run a msguniq on
-# them because later msgcomm calls tends to filter out strings
-# that repeat themselves in {component}.po files.
-function smarty_extractor {
-  ME=$1
-  root=$2
-  potdir=$3
-  input=$4
-  component=$5
-
-  `dirname $ME`/smarty-extractor.php $root $root/$input >> ${potdir}/${component}.pot
-  msguniq $potdir/${component}.pot | sponge $potdir/${component}.pot
-}
+# Replace the generation timestamp in the header
+# and set the Project-Id-Version to that of the extension
+sed -e "s/NOW/$now/" \
+    -e "s/Project-Id-Version: CiviCRM/Project-Id-Version: $extname/" \
+    `dirname $0`/header > $header
 
 # create base POT file
-set -x 
-
-echo " * building ${extname}.pot"
+echo " * Building ${extname}.pot"
 cp $header $potdir/${extname}.pot
 `dirname $0`/extractor.php extension $root >> $potdir/${extname}.pot
 msguniq $potdir/${extname}.pot | sponge $potdir/${extname}.pot
 
-# FIXME: smarty extraction?
-
 rm $header
+
+echo " * All done"
+echo ""
+echo "Generated file: $potdir/${extname}.pot"
+echo "Feel free to commit it to your git repository."
+echo ""
+echo "You can also request on the CiviCRM Extensions forum to add it to Transifex:"
+echo "[URL here]"
+echo ""
+echo "For more information:"
+echo "[wiki URL here]"
+
