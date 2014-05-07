8a13eebc1b27240748c3d6100b051c62920aafe2
Mathieu Lutfy <mathieu@bidon.ca>
2014-05-07 11:44:54 -0400

    sync-translated.sh: do not sync both ways. One way only. Too risky/confusing otherwise.

--- bin/sync-translated.sh
+++ bin/sync-translated.sh
@@ -2,14 +2,14 @@
 
 function usage() {
   cat <<EOT
-sync-translated.sh - Copies translated strings from one translation
-for untranslated strings of the other translation. Synchronizes both ways.
+sync-translated.sh - Copies translated strings from one language
+to untranslated strings of the other language.
 
 Usage:
   ./bin/sync-translated.sh [src] [dst]
 
 Example:
-  ./bin/sync-translated.sh fr fr_CA
+  ./bin/sync-translated.sh fr_FR fr_CA
 EOT
 
   exit 1;
@@ -22,19 +22,20 @@ echo "porting from $1 to $2"
 
 pots=`for pot in po/pot/*.pot; do basename $pot .pot; done`
 for pot in $pots; do
-  echo -n $pot
+  echo $pot
   msgcat --use-first po/$2/$pot.po po/$1/$pot.po | sponge po/$2/$pot.po
-  msgmerge --update po/$2/$pot.po po/pot/$pot.pot
-  msgattrib --no-obsolete po/$2/$pot.po | sponge po/$2/$pot.po
+  # msgmerge --update po/$2/$pot.po po/pot/$pot.pot
+  # msgattrib --no-obsolete po/$2/$pot.po | sponge po/$2/$pot.po
 done
 
-echo
-echo "porting from $2 to $1"
-
-pots=`for pot in po/pot/*.pot; do basename $pot .pot; done`
-for pot in $pots; do
-  echo -n $pot
-  msgcat --use-first po/$1/$pot.po po/$2/$pot.po | sponge po/$1/$pot.po
-  msgmerge --update po/$1/$pot.po po/pot/$pot.pot
-  msgattrib --no-obsolete po/$1/$pot.po | sponge po/$1/$pot.po
-done
+# [ML] 2014-05-07 do not sync both way, too risky.
+# echo
+# echo "porting from $2 to $1"
+#
+# pots=`for pot in po/pot/*.pot; do basename $pot .pot; done`
+# for pot in $pots; do
+#   echo -n $pot
+#   msgcat --use-first po/$1/$pot.po po/$2/$pot.po | sponge po/$1/$pot.po
+#   msgmerge --update po/$1/$pot.po po/pot/$pot.pot
+#   msgattrib --no-obsolete po/$1/$pot.po | sponge po/$1/$pot.po
+# done
